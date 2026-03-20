5057dd8e5a0cff39e5af3bdd912115f3ffed9018
mizunoyoukan <148046516+mizunoyoukan@users.noreply.github.com>
2026-03-20 09:51:19 +0900

    Add files via upload

--- panel.js
+++ panel.js
@@ -1,6 +1,8 @@
 let fontsize = 16;
 const textbox = document.getElementById('textbox');
 const fontSize_Input = document.getElementById('fontSizeInput'); 
+const loadtxtInput = document.getElementById('loadtxt');
+
 
 function resizeToWindow(){
     document.getElementById('textbox').style.width = (window.innerWidth - 15) + 'px';
@@ -26,3 +28,26 @@ document.getElementById('fontSizeInput').addEventListener('input', function(){
     const size = this.value;
     document.getElementById('textbox').style.fontSize = size + 'px';
 })
+
+document.getElementById('loadtxt').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    
+    if (!file) return;
+    if (!file.type.startsWith('text/') && !file.name.endsWith('.txt')){
+        alert("textfile only");
+        return;
+    }
+
+    const reader = new FileReader();
+
+    reader.onload = function(e){
+        textbox.value = e.target.result;
+    };
+
+    reader.onerror = function(e){
+        console.error("error",e);
+        alert("sorry error")
+    };
+    
+    reader.readAsText(file, 'UTF-8');
+});
